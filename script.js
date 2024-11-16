c29dd14d539ea32367270b6811bfd64e1a6afcba
Lancaster07 <eulerlancaster22@gmail.com>
2024-11-15 22:21:25 -0300

    Projeto-Cafeteria

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.querySelector(".fomurario")
+const mascara = document.querySelector(".mascara-formulario")
+
+
+
+function apareceFormu(){
+   form.style.left = "50%"
+   form.style.transform = "translateX(-50%)"
+   mascara.style.visibility ="visible" 
+}
+
+function desaparecerForm(){
+   form.style.left = "-290px"
+   form.style.transform = "translateX(0)"
+   mascara.style.visibility ="hidden" 
+}
